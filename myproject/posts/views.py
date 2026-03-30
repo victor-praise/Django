90675d0de6cd6d6fc22461c4b64a65bc38537ebb
victor-praise <61267951+victor-praise@users.noreply.github.com>
2026-03-29 22:09:33 -0400

    basic setup

---
+++ myproject/posts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Post
+# Create your views here.
+
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/posts_lists.html', {'posts': posts})
